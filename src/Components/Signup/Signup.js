328f44d6310167ce28ce0166277dc7ca93f8881a
Felix Ondullah <felixondullah@gmail.com>
2023-03-01 10:18:09 +0300

    Completed the signup stlyling

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import backgroundImage from "../Assets/bg1.jpg";
+import "../Signup/Signup.css";
 
 const Signup = () => {
   return (
     <div
       style={{
-        backgroundImage: 'url("your-image-url.jpg")',
+        backgroundImage: `url(${backgroundImage})`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         minHeight: "100vh",
@@ -14,28 +16,28 @@ const Signup = () => {
         justifyContent: "center",
       }}
     >
-      <Container className="p-4 rounded bg-light">
+      <Container className="p-4 rounded bg-custom">
         <Row className="justify-content-center">
           <Col md={6}>
             <h1 className="text-center mb-4">Sign Up</h1>
-            <Form>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Username</Form.Label>
+            <Form className="bg-transparent">
+              <Form.Group controlId="formBasicEmail" className="mb-4">
+                <Form.Label style={{ fontWeight: 'bold' }} >Username</Form.Label>
                 <Form.Control type="text" placeholder="Enter username" />
               </Form.Group>
 
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
+              <Form.Group controlId="formBasicEmail" className="mb-4">
+                <Form.Label style={{ fontWeight: 'bold' }}>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
+              <Form.Group controlId="formBasicPassword" className="mb-4">
+                <Form.Label style={{ fontWeight: 'bold' }}>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Confirm Password</Form.Label>
+              <Form.Group controlId="formBasicPassword" className="mb-4">
+                <Form.Label style={{ fontWeight: 'bold' }}>Confirm Password</Form.Label>
                 <Form.Control type="password" placeholder="Confirm Password" />
               </Form.Group>
 
