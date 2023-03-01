3a15dc07db5b37e5394393ad0233e52e2cf9f9e6
Felix Ondullah <felixondullah@gmail.com>
2023-03-01 09:35:56 +0300

    created signup form

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,6 +1,47 @@
-import React from "react";
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-function Signup() {
-  return <div></div>;
-}
+const Signup = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: 'url("your-image-url.jpg")',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Container className="p-4 rounded bg-light">
+        <Row className="justify-content-center">
+          <Col md={6}>
+            <h1 className="text-center mb-4">Sign Up</h1>
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit" block>
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
 export default Signup;
+
